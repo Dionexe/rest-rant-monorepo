59338044e7691877ab402dad0d35362a7e706e17
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-05-06 20:44:16 -0700

    Added Login Auth

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
-
+console.log(process.env.DB_USERNAME)
+console.log(process.env.DB_PASSWORD)
+console.log(process.env.DB_DATABASE)
 module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
+    "port": 5179,
     "dialect": "postgres"
   },
   "test": {
@@ -23,3 +26,4 @@ module.exports = {
     "dialect": "mysql"
   }
 }
+
